71712fd3cea520f6b3bd349feff6ca6d0d4a1b51
sbakhmetzyanov <serezha.akhmetzyanov@yandex.ru>
2024-03-17 19:27:09 +0300

    second hw

---
+++ hw2/src/triange.py
@@ -0,0 +1,19 @@
+from figure import Figure
+
+
+class Triangle(Figure):
+    def __init__(self, side_a, side_b, side_c):
+        super().__init__(name="Triangle")
+        if side_a <= 0 or side_b <= 0 or side_c <= 0:
+            raise ValueError("Стороны должны быть больше нуля")
+        self.side_a = side_a
+        self.side_b = side_b
+        self.side_c = side_c
+
+    def get_area(self):
+        s = (self.side_a + self.side_b + self.side_c) / 2
+        area = (s * (s - self.side_a) * (s - self.side_b) * (s - self.side_c)) ** 0.5
+        return area
+
+    def get_perimeter(self):
+        return self.side_a + self.side_b + self.side_c
